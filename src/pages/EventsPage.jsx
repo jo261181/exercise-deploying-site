5559bfb0eb438d78ab3d4748c60338a237a221e2
jo261181 <jlavanderburgt@gmail.com>
2026-05-15 11:26:07 +0200

    Change background color of event cards to red.50 in EventsPage component

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -111,7 +111,7 @@ export const EventsPage = () => {
               key={evt.id}
               w="100%"
               borderRadius="lg"
-              bg="whiteAlpha.800"
+              bg="red.50"
               alignItems="center"
               mb={5}
               cursor="pointer"
